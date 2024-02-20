56388b394a447395abe0cd76ec05d98c326bce18
Thomas Rice <ricethomasj@gmail.com>
2024-02-20 15:59:51 -0600

    Added API calls for gardens

---
+++ api/gardenData.js
@@ -0,0 +1,85 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getGardens = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/gardens`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getUserGardens = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/gardens?uid=${uid}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const getSingleGarden = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/gardens/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const createGarden = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/gardens`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const updateGarden = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/gardens`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+const deleteGarden = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/gardens/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(resolve)
+    .catch(reject);
+});
+
+export {
+  getGardens,
+  getSingleGarden,
+  getUserGardens,
+  createGarden,
+  updateGarden,
+  deleteGarden,
+};
